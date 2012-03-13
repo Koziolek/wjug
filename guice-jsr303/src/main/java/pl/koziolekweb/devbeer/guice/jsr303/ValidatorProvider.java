e71bb31b7f7e0a49b7aeb9ddb949b39fd2415f6c
Koziolek <bjkuczynski@gmail.com>
2012-03-13 18:11:17 +0100

    szablon konfiguracji

--- guice-jsr303/src/main/java/pl/koziolekweb/devbeer/guice/jsr303/ValidatorProvider.java
+++ guice-jsr303/src/main/java/pl/koziolekweb/devbeer/guice/jsr303/ValidatorProvider.java
@@ -1,13 +1,27 @@
 package pl.koziolekweb.devbeer.guice.jsr303;
 
 import javax.inject.Provider;
+import javax.validation.Configuration;
+import javax.validation.ConstraintValidatorFactory;
+import javax.validation.MessageInterpolator;
+import javax.validation.TraversableResolver;
+import javax.validation.Validation;
 import javax.validation.Validator;
 
 public class ValidatorProvider implements Provider<Validator> {
 
+	private Provider<ConstraintValidatorFactory> constraintValidatorFactoryProvider;
+	private Provider<MessageInterpolator> interpolatorProvider;
+	private Provider<TraversableResolver> traversableResolverProvider;
+
 	public Validator get() {
-		// TODO Auto-generated method stub
-		return null;
+		Configuration<?> configure = Validation.byDefaultProvider().configure();
+		configure
+				.constraintValidatorFactory(constraintValidatorFactoryProvider.get());
+		configure.messageInterpolator(interpolatorProvider.get());
+		configure.traversableResolver(traversableResolverProvider.get());
+
+		return configure.buildValidatorFactory().getValidator();
 	}
 
 }
